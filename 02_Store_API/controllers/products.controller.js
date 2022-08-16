038116ebe30af5fbf731aa61fec5f3ff3d2677eb
Sagar-Gondage <sagargondage370@gmail.com>
2022-08-16 23:45:22 +0530

    Product.find functionality added

--- 02_Store_API/controllers/products.controller.js
+++ 02_Store_API/controllers/products.controller.js
@@ -1,10 +1,16 @@
+const Product = require("../models/product.model");
+
 //// if we ues npm install express-async-errors then there is no need to use try catch block
 //// in this project we have used it.
 //// the project in which we have not used is the 01-task-manager
 
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("testing async errors");
-  res.status(200).json({ msg: "products testing route" });
+  // {{URL}}/products/static --> use in postman
+  const products = await Product.find({
+    featured: true,
+  });
+  // throw new Error("testing async errors");
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
--- 02_Store_API/populate.js
+++ 02_Store_API/populate.js
@@ -11,9 +11,11 @@ const start = async () => {
     //// we are using delete many just to make sure that before you enter any value database is clear.
     await Product.deleteMany();
     await Product.create(jsonProducts);
-    console.log("success");
+    //// we have added to exit to terminate the terminal once data is populated to database.
+    process.exit(0);
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
 };
 
