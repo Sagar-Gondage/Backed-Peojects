dada889da454f56bb4c60474bfe224d3fc78254a
Sagar-Gondage <sagargondage370@gmail.com>
2022-08-24 16:45:16 +0530

    delete job added

--- 03_Jobs_API/controllers/jobs.js
+++ 03_Jobs_API/controllers/jobs.js
@@ -53,7 +53,18 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete Job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOneAndRemove({ _id: jobId, createdBy: userId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 module.exports = {
